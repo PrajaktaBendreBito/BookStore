106a999a0e9b155e6c2685a16695f0af809d4f08
PrajaktaBendreBito <prajakta.bendre@bito.ai>
2025-06-26 16:39:13 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,63 @@
+const user = new Object();
+const numbers = new Array(1, 2, 3);
+
+const message = "Hello, world!";
+const greeting = "Hello, " + name;
+
+const getUser = function(id) {
+  return database.findUser(id);
+};
+
+const add = function(a, b) {
+  return a + b;
+};
+
+function Animal(name) {
+  this.name = name;
+}
+
+const getData = require('./data');
+
+console.log(user["name"]);
+
+const age = 30;
+let firstName;
+
+if (a == b) doSomething();
+
+if (isValid) doSomethingElse();
+
+let status = "active";
+if (status === "active") {
+  console.log("Active");
+} else if (status === "inactive") {
+  console.log("Inactive");
+} else if (status === "pending") {
+  console.log("Pending");
+}
+
+const person = {
+  name: "Bob",
+  age: 40
+}
+
+let score = "100" * 1;
+
+const User_profile = function() {
+  this.name = "John";
+};
+
+class Account {
+  constructor(balance) {
+    this._balance = balance;
+  }
+  balance() {
+    return this._balance;
+  }
+}
+
+const button = $(".button");
+
+const userData = { name: "Alice", age: 25 };
+const name = userData.name;
+const ageAgain = userData.age
